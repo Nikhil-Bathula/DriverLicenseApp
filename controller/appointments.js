1f220a943a409904261e0286d9df5f41a7fba2ce
Nikhil-Bathula <33082297+Nikhil-Bathula@users.noreply.github.com>
2022-12-15 06:52:32 -0500

    Auth changes

--- controller/appointments.js
+++ controller/appointments.js
@@ -1,27 +1,35 @@
 const Appointments = require("../models/Appointments");
 const User = require("../models/PersonalInfo");
 
-const AppointmentsPage = async(req, res) => {
+const AppointmentsPage = async (req, res) => {
+  if (req.session.userType === "driver") {
+    res.redirect("/");
+  } else {
     const appointments = [];
     const allAppointmentstime = [];
     const allAppointments = await Appointments.find({});
     const stringData = JSON.stringify(allAppointments);
-    res.render("appointments", { allAppointmentstime, stringData, appointments });
-}
-
-const createAppointment = (req, res) => {
-    Appointments.create(req.body)
-    res.redirect("/");
+    res.render("appointments", {
+      allAppointmentstime,
+      stringData,
+      appointments,
+    });
+  }
 };
 
-const getDataByTestResult = async(req, res) => {
-    const appointments = await User.find({
-        testStatus: { $eq: req.body.testStatus }
-    }).populate("appointmentID");
-    const allAppointmentstime = [];
-    const allAppointments = await Appointments.find({});
-    const stringData = JSON.stringify(allAppointments);
-    res.render('appointments', { appointments, stringData, allAppointmentstime })
-}
+const createAppointment = (req, res) => {
+  Appointments.create(req.body);
+  res.redirect("/");
+};
+
+const getDataByTestResult = async (req, res) => {
+  const appointments = await User.find({
+    testStatus: { $eq: req.body.testStatus },
+  }).populate("appointmentID");
+  const allAppointmentstime = [];
+  const allAppointments = await Appointments.find({});
+  const stringData = JSON.stringify(allAppointments);
+  res.render("appointments", { appointments, stringData, allAppointmentstime });
+};
 
 module.exports = { AppointmentsPage, createAppointment, getDataByTestResult };
--- controller/examiner.js
+++ controller/examiner.js
@@ -1,39 +1,53 @@
 const User = require("../models/PersonalInfo");
 
-const redirectToEmployer = async(req, res) => {
+const redirectToEmployer = async (req, res) => {
+  if (req.session.userType === "driver") {
+    res.redirect("/");
+  } else {
     const showPopup = false;
     const appointments = await User.find({
-        appointmentID: { $ne: null },
+      appointmentID: { $ne: null },
     }).populate("appointmentID");
     // console.log(appointments, 'appointments');
     res.render("examiner", { appointments, showPopup });
+  }
 };
 
-const getUserDetails = async(req, res) => {
-    const showPopup = true;
-    const appointments = await User.find({
-        appointmentID: { $ne: null },
-    }).populate("appointmentID");
-    const userDetails = await User.findOne({
-        _id: req.query.id,
-    });
-    console.log(appointments, userDetails, 'params')
-    res.render('examiner', { appointments, userDetails, showPopup });
-}
+const getUserDetails = async (req, res) => {
+  const showPopup = true;
+  const appointments = await User.find({
+    appointmentID: { $ne: null },
+  }).populate("appointmentID");
+  const userDetails = await User.findOne({
+    _id: req.query.id,
+  });
+  console.log(appointments, userDetails, "params");
+  res.render("examiner", { appointments, userDetails, showPopup });
+};
 
-const closePopup = async(req, res) => {
-    const showPopup = false;
-    const updateComment = await User.findByIdAndUpdate({ _id: req.body.id }, { $set: { comments: req.body.comment, testStatus: req.body.testStatus } });
-    const appointments = await User.find({
-        appointmentID: { $ne: null },
-    }).populate("appointmentID");
-    res.render('examiner', { appointments, showPopup });
-}
+const closePopup = async (req, res) => {
+  const showPopup = false;
+  const updateComment = await User.findByIdAndUpdate(
+    { _id: req.body.id },
+    { $set: { comments: req.body.comment, testStatus: req.body.testStatus } }
+  );
+  const appointments = await User.find({
+    appointmentID: { $ne: null },
+  }).populate("appointmentID");
+  res.render("examiner", { appointments, showPopup });
+};
 
-const getDataByTestType = async(req, res) => {
-    const showPopup = false;
-    const appointments = await User.find({ testType: req.body.type }).populate("appointmentID");
-    res.render('examiner', { appointments, showPopup });
-}
+const getDataByTestType = async (req, res) => {
+  const showPopup = false;
+  const appointments = await User.find({ testType: req.body.type }).populate(
+    "appointmentID"
+  );
+  res.render("examiner", { appointments, showPopup });
+};
 
-module.exports = { redirectToEmployer, getUserDetails, closePopup, getDataByTestType };
+module.exports = {
+  redirectToEmployer,
+  getUserDetails,
+  closePopup,
+  getDataByTestType,
+};
